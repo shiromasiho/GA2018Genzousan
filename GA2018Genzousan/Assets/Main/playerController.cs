77955d39d6f293df0f6d9907b5aae85d62c7477c
KBC11605063\kbc11605063 <shiho-s-16d@stu.kbc.ac.jp>
2018-05-11 13:09:34 +0900

    main

---
+++ GA2018Genzousan/Assets/Main/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//←に動かす
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			transform.Translate(-3,0,0);
+		}
+		//→に動かす
+		if(Input.GetKeyDown(KeyCode.RigntArrow)){
+			transform.Translate(3,0,0);
+		}
+	}
+}
