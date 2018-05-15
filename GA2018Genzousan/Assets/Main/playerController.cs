1c1162f5b477b966384127b8f237ed68d586e218
KBC11605063\kbc11605063 <shiho-s-16d@stu.kbc.ac.jp>
2018-05-15 11:37:32 +0900

    player move

--- GA2018Genzousan/Assets/Main/playerController.cs
+++ GA2018Genzousan/Assets/Main/playerController.cs
@@ -2,22 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class playerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+        Rigidbody2D rigid2D;
+        Animator animator;
+        float jumpForce = 680.0f;
+        float walkForce = 30.0f;
+        float maxWalkSpeed = 2.0f;
+
+
+    void Start(){
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
 	
-	// Update is called once per frame
 	void Update () {
-        ////←に動かす
-        //if(Input.GetKeyDown(KeyCode.LeftArrow)){
-        //    transform.Translate(-3,0,0);
-        //}
-        ////→に動かす
-        //if(Input.GetKeyDown(KeyCode.RigntArrow)){
-        //    transform.Translate(3,0,0);
-    //    }
+        //ジャンプ 
+        if (Input.GetKeyDown(KeyCode.UpArrow) &&
+            this.rigid2D.velocity.y == 0)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+        //移動 
+        int Key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) Key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) Key = -1;
+
+        //プレイヤー速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        //スピード制限
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * Key * this.walkForce);
+        }
+
     }
 }
