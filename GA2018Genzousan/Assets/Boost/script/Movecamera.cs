8f9a23ee35668b406cc496ff90dc973dc85530d3
LAPTOP-UEIRJFST\moyo0 <yuki-t-17d@stu.kbc.ac.jp>
2018-05-15 11:39:09 +0900

    Boost追加

---
+++ GA2018Genzousan/Assets/Boost/script/Movecamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movecamera : MonoBehaviour {
+
+    GameObject Player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.Player = GameObject.Find("camera");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y,-10);	
+	}
+}
