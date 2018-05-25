874e05b07b7104b6bb2d03b62aa7440b23278703
KBC11605063\kbc11605063 <shiho-s-16d@stu.kbc.ac.jp>
2018-05-25 11:34:34 +0900

    help(fire tukaunaratukatte

---
+++ GA2018Genzousan/Assets/Help/FireGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGenerator : MonoBehaviour {
+
+    public GameObject firePrefab;
+    float span = 1.0f;
+    float delta = 0;
+	
+	// Update is called once per frame
+    void Update(){
+        //弾生成
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span){
+            this.delta =0;
+            GameObject go = Instantiate(firePrefab) as GameObject;
+            int px = Random.Range(-3, 4);
+            go.transform.position = new Vector3(15,px,0);
+            }
+	}
+}
