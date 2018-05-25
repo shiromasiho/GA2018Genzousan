8e8ba5195e106ee6db4c29762bbf49a939e33502
KBC11606024\kbc11606024 <yuma-k-16d@stu.kbc.ac.jp>
2018-05-25 09:36:48 +0900

    やりおるマンだぁぁぁぁぁぁぁぁぁぁ！！

---
+++ GA2018Genzousan/Assets/Clear/Hellotama.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hellotama : MonoBehaviour {
+    int bulletflg;
+    public GameObject tama;
+    public GameObject bullet;
+	// Use this for initialization
+	void Start () {
+        bulletflg = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("a")&&bulletflg == 1)
+        {
+            Debug.Log("弾丸にゃ");
+            Instantiate(bullet);
+            bulletflg = 0;
+        }
+	}
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(Input.GetKeyDown("b")&&bulletflg ==0)
+        {
+            if (other.tag == "tama")
+            {
+            Debug.Log("撃たれてんじゃねぇか！！！");
+            Destroy(tama);
+            bulletflg = 1;
+            }
+        }
+    }
+}
