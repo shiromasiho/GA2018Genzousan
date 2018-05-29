de4c8d6a7c87c557ae5ce725910124a158d8c60d
LAPTOP-ERHGB55S\k.marbow <masatoshi-k-17e@stu.kbc.ac.jp>
2018-05-29 16:49:55 +0900

    Home処理追加　Homeフォルダ作成　ホームからタイトルへの画面遷移

---
+++ GA2018Genzousan/Assets/Home/HomeDirector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeDirector : MonoBehaviour {
+
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("Title Scene");
+    }
+}
