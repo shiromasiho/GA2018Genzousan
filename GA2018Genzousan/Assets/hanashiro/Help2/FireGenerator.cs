7119911811ae9bfc97775e72b7e86c03a5aea77d
DESKTOP-6MO8KG5\owner-taiki <taiki-h-17e@stu.kbc.ac.jp>
2018-05-31 14:20:24 +0900

    ヘルプ　キャラクター移動、stock機能

---
+++ GA2018Genzousan/Assets/hanashiro/Help2/FireGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGenerator : MonoBehaviour {
+
+    public GameObject firePrefab;
+    float span = 4.0f;
+    float delta = 0;
+    public GameObject go;
+    public GameObject playerfirefab;
+    public GameObject go2;
+    public static int Pfireflg = 0;
+	// Update is called once per frame
+    void Update(){
+        //弾生成
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span){ //エネミーファイア
+            this.delta =0;
+             go = Instantiate(firePrefab) as GameObject;
+           // int px = Random.Range(-3, 4);
+            go.transform.position = new Vector3(20,-3,0); //炎の位置
+        }
+
+
+        if (Pfireflg == 1)  //プレイヤーファイア
+        { 
+            go2 = Instantiate(playerfirefab) as GameObject;
+            Pfireflg = 0;
+
+        }
+
+	}
+}
---
+++ GA2018Genzousan/Assets/hanashiro/Help2/HelpPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpPlayer : MonoBehaviour {
+
+    Rigidbody2D rigid2D;
+    Animator animator;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // ジャンプ 
+        if (Input.GetKeyDown(KeyCode.UpArrow) &&
+            this.rigid2D.velocity.y == 0){
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        //移動 
+        int Key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) Key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) Key = -1;
+
+        //プレイヤー速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        //スピード制限
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * Key * this.walkForce);
+        }
+	}
+}
