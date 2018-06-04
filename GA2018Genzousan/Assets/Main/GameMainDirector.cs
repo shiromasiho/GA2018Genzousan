88c6ffc2d2931a633b336fdad62a5f2bdab040ec
KBC11605063\kbc11605063 <shiho-s-16d@stu.kbc.ac.jp>
2018-06-04 15:37:59 +0900

    sceneとぶんご

---
+++ GA2018Genzousan/Assets/Main/GameMainDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //LoadScene を使うために必要
+
+public class GameMainDirector : MonoBehaviour {
+
+    public int playerHp;
+    public int enemyHp;
+    // Use this for initialization
+    void Start()
+    {
+        playerHp = 1;
+        enemyHp = 30;
+    }
+	// Update is called once per frame
+	void Update () {
+       // if (Input.GetKeyDown("a"))
+        if (enemyHp == 0)
+        {
+            SceneManager.LoadScene("Clear Scene");
+        }
+       // if (Input.GetKeyDown("s"))
+        if (playerHp == 0)
+        {
+            SceneManager.LoadScene("OverScene");
+        }
+	}
+}
