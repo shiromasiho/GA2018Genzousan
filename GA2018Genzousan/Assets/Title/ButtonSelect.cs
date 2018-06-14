737249364937f52940a96df67b685532400c5c52
LAPTOP-ERHGB55S\k.marbow <masatoshi-k-17e@stu.kbc.ac.jp>
2018-06-14 16:44:22 +0900

    Home/TitleシーンにてBGMとSE追加　ゲームオーバー用アイコンをimagesフォルダに追加

--- GA2018Genzousan/Assets/Title/ButtonSelect.cs
+++ GA2018Genzousan/Assets/Title/ButtonSelect.cs
@@ -41,6 +41,7 @@ public class ButtonSelect : MonoBehaviour {
                 Debug.Log(FadeManager);
                 FadeManager.namae = "Main Scene";
                 FadeManager.Instance.LoadScene("Main Scene", 1.0f);
+                
                 break;
             case 2:
                 FadeManager.namae = "Help Scene";
