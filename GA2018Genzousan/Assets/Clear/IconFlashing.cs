b22e9d17baa924d1c005108479841bab020a7d50
KBC11605035\kbc11605035 <hiraki.199749@gmail.com>
2018-06-28 15:04:03 +0900

    Clear Scene変更

---
+++ GA2018Genzousan/Assets/Clear/IconFlashing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconFlashing : MonoBehaviour {
+
+    float IconFTime = 90.0f;                    // 点滅時間
+	
+	void Update () {
+        Vector3 pos = transform.position;
+        IconFTime--;
+
+        if (IconFTime >= 30) pos.z = 0;         // 表示
+         else                pos.z = 1;         // 非表示
+
+        if (IconFTime < 0) IconFTime = 90;
+
+        transform.position = pos;
+	}
+}
